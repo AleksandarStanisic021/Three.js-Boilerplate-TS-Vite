235d21b343e851124215f9b94a18d95c2b52c6b4
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-23 15:47:49 +0200

    positionLocal

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import * as THREE from "three/webgpu";
-import { color, texture, convertColorSpace } from "three/tsl";
+import { abs, Fn, If, positionLocal, rotateUV, time, vec2 } from "three/tsl";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
@@ -12,6 +12,20 @@ const camera = new THREE.PerspectiveCamera(
   10,
 );
 camera.position.z = 1;
+
+const main = Fn(() => {
+  const p = positionLocal.toVar();
+
+  If(abs(p.x).greaterThan(0.45), () => {
+    // @ts-ignore
+    p.z = 1;
+  });
+  If(abs(p.y).greaterThan(0.45), () => {
+    // @ts-ignore
+    p.z = 1;
+  });
+  return p;
+});
 
 const renderer = new THREE.WebGPURenderer({
   antialias: true,
@@ -29,13 +43,8 @@ window.addEventListener("resize", function () {
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-let material = new THREE.NodeMaterial();
-//material.fragmentNode = color("crimson");
-material.fragmentNode = convertColorSpace(
-  texture(new THREE.TextureLoader().load("https://sbcode.net/img/grid.png")),
-  THREE.SRGBColorSpace,
-  THREE.LinearSRGBColorSpace,
-);
+const material = new THREE.NodeMaterial();
+material.fragmentNode = main();
 
 const mesh = new THREE.Mesh(new THREE.PlaneGeometry(), material);
 scene.add(mesh);
