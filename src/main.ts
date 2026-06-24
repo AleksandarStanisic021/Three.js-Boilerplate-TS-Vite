95e46cd3fbb97a7a7a943ed900054c4e9b4239ca
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-24 20:39:44 +0200

    restart scene

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,40 @@
 import "./style.css";
-import * as THREE from "three";
+import * as THREE from "three/webgpu";
+import { color } from "three/tsl";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-console.log(THREE);
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10,
+);
+camera.position.z = 1;
+
+const renderer = new THREE.WebGPURenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const material = new THREE.NodeMaterial();
+material.fragmentNode = color("blue");
+
+const mesh = new THREE.Mesh(new THREE.PlaneGeometry(), material);
+scene.add(mesh);
+
+function animate() {
+  controls.update();
+
+  renderer.render(scene, camera);
+}
