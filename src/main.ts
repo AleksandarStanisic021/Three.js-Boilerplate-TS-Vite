02d232d834dea2a733070929b40750011a5cbd31
Aleksandar Stanisic <ciandre23@gmail.com>
2026-06-24 22:00:40 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three/webgpu";
 import { positionLocal } from "three/tsl";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { color } from "three/src/nodes/TSL.js";
 
 const scene = new THREE.Scene();
 
@@ -30,13 +31,13 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 const material = new THREE.NodeMaterial();
-// material.fragmentNode = color('crimson')
+material.fragmentNode = color("crimson");
 // material.fragmentNode = convertColorSpace(
 //   texture(new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')),
 //   THREE.SRGBColorSpace,
 //   THREE.LinearSRGBColorSpace
 // )
-material.fragmentNode = positionLocal;
+//material.fragmentNode = positionLocal;
 
 const mesh = new THREE.Mesh(new THREE.PlaneGeometry(), material);
 scene.add(mesh);
